0ed84730ee10f052c5f956e68968312cfdce7225
Sebastian Bolanos <sebasheston@gmail.com>
2018-03-26 20:57:20 -0700

    some fancy scripting going on here

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
-var elements =  document.getElementsByTagName("a");
+var elements =  document.getElementsByTagName('a');
 
-for (var i=0, max=elements.lenght; i<max; i++){
-	console.log(elements[i])
+for (i=0; i < elements.length; i++){
+
+	var test = elements[i].toString();
+	if (test.includes("mp4") || (test.includes("mp3"))){
+		mediaPath = elements[i].href;
+		indexM = mediaPath.indexOf("files/");
+		mediaPath = mediaPath.substring(indexM, mediaPath.lenght);
+		elements[i].href="http://192.168.1.18/media/media.html?file="+ mediaPath;
+	}
 }
